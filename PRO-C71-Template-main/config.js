605a24cda787e197d64a7e318445314a2398df26
Hemanth-L <67827171+Hemanth-L@users.noreply.github.com>
2022-01-22 11:51:19 -0600

    Update config.js

--- PRO-C71-Template-main/config.js
+++ PRO-C71-Template-main/config.js
@@ -3,7 +3,13 @@ require("@firebase/firestore");
 
 //Paste your firebaseConfig here
 
-
+apiKey: "AIzaSyBt3O8hd_sENO5SX0z0PbXl7LETD4QOSVk",
+    authDomain: "e-ride-70d06.firebaseapp.com",
+    projectId: "e-ride-70d06",
+    storageBucket: "e-ride-70d06.appspot.com",
+    messagingSenderId: "733481354322",
+    appId: "1:733481354322:web:a4041d0d7635fb205b0b8b",
+    measurementId: "G-65FT1D20FH"
 
 
 
